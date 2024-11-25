2576cd9c12ffae3e2421bef5e0020aacea3106d8
bowlarbear <bowlarbear@proton.me>
2024-11-25 16:55:19 -0500

    rework;

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,46 @@ type Cords = (u8, u8);
 
 // Ship Trait
 trait Ship {
-    fn update_pos(&mut self, current_position: Cords) -> Option<Cords>;
+    // fn update_pos(&mut self, current_position: Cords) -> Option<Cords>;
 
-    fn ship_tick(&mut self, current_position: Cords) -> Option<Cords> {
-        self.update_pos(current_position)
-    }
+    // fn ship_tick(&mut self, current_position: Cords) -> Option<Cords> {
+    //     self.update_pos(current_position)
+    // }
+
+    fn get_ai(&mut self) -> Option<&mut ShipAI>;
 
     fn symbol(&self) -> char;
+}
+
+enum ShipAction{
+    Move(i8, i8),
+    None,
+}
+
+struct ShipAI{
+    current_tick: u64,
+    tick_interval: u64,
+    actions: Vec<ShipAction>,
+};
+
+
+impl ShipAI{
+    fn new(tick_interval: u64) -> Self {
+        ShipAI{
+            current_tick: 0,
+            tick_interval: tick_interval,
+            actions: vec![ShipAction::Move(1,0)],
+        }
+    }
+
+    fn tick(&mut self) -> ShipAction{
+        self.current_tick = (self.current_tick + 1) % self.tick_interval;
+        if self.current_tick == 0 {
+            self.actions[0]
+        } else {
+            ShipAction::None
+        }
+    }
 }
 
 // --- PlayerShip Struct ---
@@ -27,26 +60,12 @@ struct PlayerShip;
 
 impl Ship for PlayerShip{
     //detect player inputs
-    fn update_pos(&mut self, current_position: Cords) -> Option<Cords>{
-        if event::poll(Duration::from_millis(10)).unwrap() {
-            if let Event::Key(key_event) = event::read().unwrap(){
-                match key_event.code{
-                    KeyCode::Left => {
-                        if current_position.0 > 0 {
-                            return Some((current_position.0 - 1, GRID_HEIGHT - 1));
-                        }
-                    }
-                    KeyCode::Right => {
-                        if current_position.0 < GRID_WIDTH - 1 {
-                            return Some((current_position.0 + 1, GRID_HEIGHT - 1));
-                        }
-                    }
-                    _ => {}
-                }
-            }
-        }
-        //stay in the same spot if no user input
-        Some(current_position)
+    // fn update_pos(&mut self, current_position: Cords) -> Option<Cords>{
+    //     Some(current_position)
+    // }
+
+    fn get_ai(&mut self) -> Option<&mut ShipAI>{
+        None
     }
 
     fn symbol(&self) -> char{
@@ -58,15 +77,20 @@ impl Ship for PlayerShip{
 struct FlyShip {
     time_stationary: u32,
     past_positions: Vec<Cords>,
+    ship_ai: ShipAI,
 }
 
 impl Ship for FlyShip {
-    fn update_pos(&mut self, current_position: Cords) -> Option<Cords> {
-        self.time_stationary = (self.time_stationary + 1) % 10;
-        if self.time_stationary == 9 {
-            let new_y = (current_position.1 + 1) % GRID_HEIGHT;
-            Some((current_position.0, new_y))
-        } else {None}
+    // fn update_pos(&mut self, current_position: Cords) -> Option<Cords> {
+    //     self.time_stationary = (self.time_stationary + 1) % 10;
+    //     if self.time_stationary == 9 {
+    //         let new_y = (current_position.1 + 1) % GRID_HEIGHT;
+    //         Some((current_position.0, new_y))
+    //     } else {None}
+    // }
+
+    fn get_ai(&mut self) -> Option<&mut ShipAI>{
+        Some(self.ship_ai)
     }
 
     fn symbol(&self) -> char{
@@ -78,11 +102,16 @@ impl Ship for FlyShip {
 struct BeeShip {
     time_stationary: u32,
     past_positions: Vec<Cords>,
+    ship_ai: ShipAI,
 }
 
 impl Ship for BeeShip {
-    fn update_pos(&mut self, current_position: Cords) -> Option<Cords> {
-        Some((current_position.0, current_position.1 + 1))
+    // fn update_pos(&mut self, current_position: Cords) -> Option<Cords> {
+    //     Some((current_position.0, current_position.1 + 1))
+    // }
+
+    fn get_ai(&mut self) -> Option<&mut ShipAI>{
+        Some(self.ship_ai)
     }
 
     fn symbol(&self) -> char{
@@ -95,17 +124,27 @@ impl Ship for BeeShip {
 struct Explosion {
     time_stationary: u32,
     past_positions: Vec<Cords>,
+    ship_ai: ShipAI,
 }
 
 impl Ship for Explosion{
 
-    fn update_pos(&mut self, current_position: Cords) -> Option<Cords>{
-        Some((current_position.0, current_position.1))
+    // fn update_pos(&mut self, current_position: Cords) -> Option<Cords>{
+    //     Some((current_position.0, current_position.1))
+    // }
+    fn get_ai(&mut self) -> Option<&mut ShipAI>{
+        Some(self.ship_ai)
     }
 
     fn symbol(&self) -> char{
         '#'
     }
+}
+
+enum GameAction {
+    Move(Cords, Cords),
+    Add(Cords, Box<dyn Ship>),
+    Remove(Cords),
 }
 
 // --- GameState Struct ---
@@ -155,19 +194,36 @@ impl GameState {
         }
     }
 
-    fn game_tick(&mut self) {
-        //collect updates for new and old positions
-        let updates: Vec<(Cords, Cords)> = self.game_board
-        .iter_mut()
-        .filter_map(|(&current_position, ship)| {
-            let new_position = ship.ship_tick(current_position)?;
-            Some((current_position, new_position))
-        })
-        .collect();
-        //apply updates
-        for (old_position, new_position) in updates {
-            self.move_ship(old_position, new_position);
-        }
+    // fn game_tick(&mut self) {
+    //     //collect updates for new and old positions
+    //     let updates: Vec<(Cords, Cords)> = self.game_board
+    //     .iter_mut()
+    //     .filter_map(|(&current_position, ship)| {
+    //         let new_position = ship.ship_tick(current_position)?;
+    //         Some((current_position, new_position))
+    //     })
+    //     .collect();
+    //     //apply updates
+    //     for (old_position, new_position) in updates {
+    //         self.move_ship(old_position, new_position);
+    //     }
+    // }
+
+    fn game_tick(&mut self){
+        let gb: &mut BTreeMap<Cords, Box<dyn Ship>> = self.game_board;
+        let iter: IterMut<(&Cords, &mut Box<dyn Ship>)> = gb.iter_mut();
+        let new_iter: IterMut<ShipAction> = iter.map(|(cords, ship): (&Cords, &mut Box<dyn Ship>)| {
+            let opt_ai: Option<&mut ShipAI> = ship.get_ai();
+            let a: Option<ShipAction> = opt_ai.map(|ai: &mut ShipAI|{
+                ai.tick()
+            }); 
+            (a.unwrap_or(ShipAction::None), cords)
+        });
+        new_iter.map(|sa: ShipAction|{
+            match sa {
+                ShipAction::Move(x, y) => GameAction::Move((), ())
+            }
+        });
     }
 }
 
@@ -180,6 +236,7 @@ fn main() {
     let flyship = FlyShip {
         time_stationary: 0,
         past_positions: Vec::new(),
+        ship_ai: ShipAI::new(10),
     };
     // Insert the FlyShip into the `ships` map
     gamestate.add_ship((0,0), Box::new(flyship));
@@ -188,7 +245,7 @@ fn main() {
         gamestate.game_tick();
         gamestate.print_frame();
         // Tick rate
-        sleep(Duration::from_millis(500));
+        sleep(Duration::from_millis(100));
     }
 }
 
